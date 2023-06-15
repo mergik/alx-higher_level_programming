96c93cc34f3f1a9c004f6a1a7ce3c36722376cbc
MR311X <maker@mgm.anonaddy.me>
2023-06-15 05:21:20 +0300

    4. Only differents

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+def only_diff_elements(set_1, set_2):
+    """Returns the symmetric difference between 2 sets"""
+    sym_diff = set_1.symmetric_difference(set_2)
+    return sym_diff
