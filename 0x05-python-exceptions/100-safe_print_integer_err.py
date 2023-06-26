6e74fc8ee0cfa274c8447a1a7723e28c5f2252a7
MR311X <maker@mgm.anonaddy.me>
2023-06-27 02:28:07 +0300

    Correction: Added extra line break

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     """Returns a boolean if integer is printed correctly"""
     try:
@@ -8,7 +9,8 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError:
-        print("Exception: Unknown format code 'd' for object of type '{}'".format(type(value).__name__), file=sys.stderr)
+        print("Exception: Unknown format code 'd' for object of type '{}'".format(type(value).__name__),
+              file=sys.stderr)
     except TypeError:
         print("Exception: '{}' is not an integer".format(value), file=sys.stderr)
     return False
