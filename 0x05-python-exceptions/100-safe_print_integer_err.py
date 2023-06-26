b5a7c28379b0de2fad89f684c237e1f926e18d7d
MR311X <maker@mgm.anonaddy.me>
2023-06-27 02:02:58 +0300

    7. Safe integer print with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        value = int(value)  # Convert value to integer if possible
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        print("Exception: Unknown format code 'd' for object of type '{}'".format(type(value).__name__), file=sys.stderr)
+    except TypeError:
+        print("Exception: '{}' is not an integer".format(value), file=sys.stderr)
+    return False
