56c16a465e7914847d5b3c2f61eaeea889ad0d12
MR311X <maker@mgm.anonaddy.me>
2023-06-27 01:41:12 +0300

    2. Print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Returns the first selected integer elements of a list"""
+    count = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end=" ")
+                count += 1
+    except (IndexError, TypeError):
+        pass
+    finally:
+        print()
+        return count
