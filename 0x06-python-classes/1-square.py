ed3fd74faefb027baa9bfef3abc7097937b9b237
MR311X <maker@mgm.anonaddy.me>
2023-06-28 02:53:59 +0300

    FIX: Added required documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initialize the Square object.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
