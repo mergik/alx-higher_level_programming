5037d98e26b318bc0c0373a1e54f3d38dceb7fed
MR311X <maker@mgm.anonaddy.me>
2023-06-28 02:37:21 +0300

    FIX: Added function comments

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self):
+        """Initialize the Square object (constructor)"""
         pass
 
     def area(self):
+        """Method to calculate the area of the square"""
         pass
 
     def perimeter(self):
+        """Method to calculate the perimeter of the square"""
         pass
