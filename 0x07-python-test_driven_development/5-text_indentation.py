c18261348288be0e2a9703f16f43919c6872ab1c
MR311X <maker@mgm.anonaddy.me>
2023-07-08 04:05:04 +0300

    4. Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module provides a function for indenting text.
+
+The `text_indentation` function indents a given text by adding 2 new lines after each of the characters '.', '?',
+and ':'.
+"""
+
+
+def text_indentation(text):
+    """
+    Indents a given text by adding 2 new lines after each of the characters '.', '?', and ':'.
+
+    Args:
+        text (str): The text to be indented.
+
+    Raises:
+        TypeError: If `text` is not a string.
+    """
+    # Check if text is a string
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    # Remove leading and trailing whitespace from the text
+    text = text.strip()
+
+    # Iterate through each character in the text
+    output = ""
+    for char in text:
+        # Add the character to the output
+        output += char
+
+        # Check if the character is ., ?, or :
+        if char in [".", "?", ":"]:
+            # Add two newlines after the character
+            output += "\n\n"
+
+    # Print the resulting indented text
+    print(output)
