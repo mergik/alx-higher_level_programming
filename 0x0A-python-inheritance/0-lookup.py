f6054ed92c7180cc1009824e05080650ad8ea337
MR311X <maker@mgm.anonaddy.me>
2023-07-10 19:48:57 +0300

    Added doc outside the lookup module

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module for returning a list of available attributes and methods of an object
+"""
+
+
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object.
