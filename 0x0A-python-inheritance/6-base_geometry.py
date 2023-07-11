26c9a25e31f74c791ca7e9b508b7c57c94626a0c
MR311X <maker@mgm.anonaddy.me>
2023-07-11 03:40:10 +0300

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Geometry related modules
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related classes.
+
+    This class provides a foundation for implementing geometry-specific functionality.
+    """
+
+    def area(self):
+        """
+        Computes the area of the geometry.
+
+        Raises:
+            Exception: This method is not implemented in the base class.
+        """
+        raise Exception("area() is not implemented")
