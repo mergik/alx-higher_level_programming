b1d8e52176a159971a9872c9c9700e1a37a5e590
MR311X <maker@mgm.anonaddy.me>
2023-07-11 04:20:50 +0300

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Geometry related modules
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class representing a square.
+
+    This class inherits from the Rectangle class and provides functionality specific to squares.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a square with the specified size.
+
+        Parameters:
+            size (int): The size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
