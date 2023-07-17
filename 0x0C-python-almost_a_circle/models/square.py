7c105286099b98e6e8598107b00e386b78991aba
MR311X <maker@mgm.anonaddy.me>
2023-07-17 19:50:37 +0300

    11. Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,14 +11,19 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """
         Initialize a Square object.
-
-        Args:
-            size (int): The size of the square (width and height).
-            x (int): The x-coordinate of the square's position.
-            y (int): The y-coordinate of the square's position.
-            id (int): The ID of the square (optional).
         """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter for the size attribute."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute."""
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Return the string representation of the square."""
