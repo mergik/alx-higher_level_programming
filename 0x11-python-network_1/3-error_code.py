a678d79b28104dfe08cdc01dcc70b66153dcea7f
mergik <gmerlix@yahoo.com>
2023-10-03 03:54:05 +0300

    Added module documentation

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,12 +3,20 @@ import urllib.request
 import urllib.error
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
 
+def display_response_content_or_error_code(url):
+    """
+    Sends a request to the given URL and displays the body of the response or
+    the HTTP error code if an error occurs.
+    """
     try:
         with urllib.request.urlopen(url) as response:
             content = response.read().decode('utf-8')
         print(content)
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    display_response_content_or_error_code(url)
