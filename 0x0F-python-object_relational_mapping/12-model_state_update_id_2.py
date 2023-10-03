673c70e260859b3b30a752644ba76ed849b1f91b
mergik <gmerlix@yahoo.com>
2023-10-03 15:36:38 +0300

    ATTEMPT 2: 12. Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -9,15 +9,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    username, password, database = argv[1:4]
-    db_uri = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
-        username, password, database
-    )
-    engine = create_engine(db_uri, pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name="Louisiana")
-    session.add(new_state)
+    state = State(name="Louisiana")
+    session.add(state)
     session.commit()
-    print(new_state.id)
+    print(state.id)
     session.close()
