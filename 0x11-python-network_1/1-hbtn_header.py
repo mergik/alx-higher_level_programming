be9c67ddec1164e65824e9f00ede8d194b745262
mergik <gmerlix@yahoo.com>
2023-10-03 04:03:02 +0300

    Added conditional statement

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,9 +6,9 @@ of the Request ID
 import urllib.request
 import sys
 
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.headers.get('X-Request-Id')
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    x_request_id = response.headers.get('X-Request-Id')
-
-print(x_request_id)
+    print(x_request_id)
