84bfafc1d88869012514261fde677ef7f7f53bab
mergik <gmerlix@yahoo.com>
2023-10-03 14:54:01 +0300

    Added class documentation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Represents a state in the database."""
     __tablename__ = 'states'
     id = Column(
         Integer, primary_key=True, autoincrement=True, nullable=False
