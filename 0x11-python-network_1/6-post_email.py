9a9ebabef73137c30567c2ca8a0d7bc0e25b88b7
mergik <gmerlix@yahoo.com>
2023-10-03 04:29:17 +0300

    7. Error code #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a parameter,
-and finally displays the body of the response.
+Python script that takes in a URL and email address,
+sends POST request to the URL with email as parameter,
+and finally displays body of the response.
 """
 import requests
 import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
